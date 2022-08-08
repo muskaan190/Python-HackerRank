bfe956b21b47dba33128a9f1e1746c631c9107c1
Muskan Makan <90406508+muskaan190@users.noreply.github.com>
2022-08-08 23:49:55 +0530

    Code added

---
+++ Whats your name.py
@@ -0,0 +1,10 @@
+# Complete the print_full_name function in the editor below.
+
+# print_full_name has the following parameters:
+
+# string first: the first name
+# string last: the last name
+
+
+def print_full_name(first, last):
+    print("Hello "+first+" "+last+"! You just delved into python.")
